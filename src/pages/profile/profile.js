2bb2577180e3c8929b18f3a3a2bd44ffe5c34939
Sergio Adonis <sergio.adonis@hotmail.com>
2021-06-07 23:15:44 -0600

    fetching usermetadata for profile

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,59 +1,59 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './profile.scss';
+// import ResponsiveBox, {
+//   Row,
+//   Col,
+//   Item,
+//   Location
+// } from 'devextreme-react/responsive-box';
 import Form from 'devextreme-react/form';
 import LoadPanel from 'devextreme-react/load-panel';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useUserMetadata } from '../../api/user-metadata';
+// import { useScreenSize } from '../../utils/media-query';
+
+const Avatar = ({ picture }) => (
+  <div className={'form-avatar'}>
+    <img alt={''} src={picture} />
+  </div>
+);
+
+const ProfileForm = ({ formData }) => (
+  <Form
+    id={'form'}
+    defaultFormData={formData}
+    labelLocation={'top'}
+    colCountByScreen={colCountByScreen}
+    readOnly={true}
+  />
+);
+
+// function screen(width) {
+//   console.log(width);
+//   return width < 600 ? 'xs' : width < 960 ? 'sm' : width < 1280 ? 'md' : 'lg';
+// }
 
 export default () => {
-  const [notes, setNotes] = useState(
-    'Sandra is a CPA and has been our controller since 2008. She loves to interact with staff so if you`ve not met her, be certain to say hi.\r\n\r\nSandra has 2 daughters both of whom are accomplished gymnasts.'
-  );
-  
   const { user } = useAuth0();
-  const { userMetadata, loading, error } = useUserMetadata({userId: user.sub});
-
-  const employee = {
-    ID: 7,
-    FirstName: 'Sandra',
-    LastName: 'Johnson',
-    Prefix: 'Mrs.',
-    Position: 'Controller',
-    Picture: user.avatar_url,
-    BirthDate: new Date('1974/11/15'),
-    HireDate: new Date('2005/05/11'),
-    Notes: notes,
-    Address: '4600 N Virginia Rd.',
-    UserMetadata: error ? error : JSON.stringify(userMetadata)
-  };
+  const { userMetadata, loading, error } = useUserMetadata({
+    userId: user.sub
+  });
+  if (error) console.log(error);
+  // const { isXSmall, isSmall, isMedium } = useScreenSize();
 
   return (
-      <React.Fragment>
-        <h2 className={'content-block'}>Profile</h2>
+    <React.Fragment>
+      <h2 className={'content-block'}>Profile</h2>
 
-        {user && user.picture && (
-          <div className={'content-block dx-card responsive-paddings'}>
-            <div className={'form-avatar'}>
-              <img alt={''} src={user.picture} />
-            </div>
-            <span>{notes}</span>
-          </div>
-        )}
-
-        {!loading ? (
-          <div className={'content-block dx-card responsive-paddings'}>
-            <Form
-              id={'form'}
-              defaultFormData={employee}
-              onFieldDataChanged={(e) =>
-                e.dataField === 'Notes' && setNotes(e.value)
-              }
-              labelLocation={'top'}
-              colCountByScreen={colCountByScreen}
-            />
-          </div>
-        ) : <LoadPanel visible={true} />}
-      </React.Fragment>
+      {!!loading ? (
+        <LoadPanel visible={true} />
+      ) : (
+        <div className={'content-block dx-card responsive-paddings'}>
+          <Avatar picture={user.picture} />
+          <ProfileForm formData={userMetadata} />
+        </div>
+      )}
+    </React.Fragment>
   );
 };
 
