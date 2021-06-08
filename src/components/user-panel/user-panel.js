8ac020e844fa531c0527ec43087b98e3db1c04ea
Sergio Adonis <sergio.adonis@hotmail.com>
2021-06-07 23:43:42 -0600

    stop logging current path

--- src/components/user-panel/user-panel.js
+++ src/components/user-panel/user-panel.js
@@ -3,19 +3,19 @@ import { useHistory } from 'react-router-dom';
 import ContextMenu, { Position } from 'devextreme-react/context-menu';
 import List from 'devextreme-react/list';
 import { useAuth0 } from '@auth0/auth0-react';
-import { useNavigation } from '../../contexts/navigation';
+// import { useNavigation } from '../../contexts/navigation';
 import './user-panel.scss';
 
 // const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 // const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 const publicUrl = process.env.PUBLIC_URL || '';
 
-export default function ({menuMode}) {
+export default function ({ menuMode }) {
   const { user, logout } = useAuth0();
   const history = useHistory();
-  const { navigationData } = useNavigation();
-  const { currentPath } = navigationData;
-  console.log(currentPath)
+  // const { navigationData } = useNavigation();
+  // const { currentPath } = navigationData;
+  // console.log(currentPath)
 
   const navigateToProfile = useCallback(
     () => history.push('/profile'),
@@ -41,7 +41,7 @@ export default function ({menuMode}) {
   return (
     <div className={'user-panel'}>
       {user && (
-      <div className={'user-info'}>
+        <div className={'user-info'}>
           <div className={'image-container'}>
             <div
               style={{
@@ -52,7 +52,7 @@ export default function ({menuMode}) {
             />
           </div>
           <div className={'user-name'}>{user.email}</div>
-      </div>
+        </div>
       )}
 
       {menuMode === 'context' && (
